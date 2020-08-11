d4e9c497c640ccc0ac72b9952197ae302f7445b0
Davo <Davo@Davo-Mac.local>
2020-08-11 14:21:24 -0600

    .gitignore fix

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-const expressSanitizer = require("express-sanitizer"),
-  methodOverride = require("method-override"),
-  bodyParser = require("body-parser"),
-  mongoose = require("mongoose"),
-  express = require("express"),
+const expressSanitizer = require('express-sanitizer'),
+  methodOverride = require('method-override'),
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose'),
+  express = require('express'),
   app = express();
 
-mongoose.connect("mongodb://localhost:27017/blog_app", {
+mongoose.connect('mongodb://localhost:27017/blog_app', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-mongoose.set("useFindAndModify", false);
+mongoose.set('useFindAndModify', false);
 
 // Tell me if we connected correctly to the DB
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
   console.log("we're connected to the DB!");
 });
 
-app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 // Tell Express to use body-parser
 app.use(
   bodyParser.urlencoded({
@@ -27,7 +27,7 @@ app.use(
   })
 );
 app.use(expressSanitizer());
-app.use(methodOverride("_method"));
+app.use(methodOverride('_method'));
 
 // Schema for DB
 const blogSchema = new mongoose.Schema({
@@ -40,21 +40,21 @@ const blogSchema = new mongoose.Schema({
   },
 });
 // DB model Config
-const Blog = mongoose.model("Blog", blogSchema);
+const Blog = mongoose.model('Blog', blogSchema);
 
 // RESTful Routes
 // Landing
-app.get("/", (req, res) => {
-  res.redirect("/blogs");
+app.get('/', (req, res) => {
+  res.redirect('/blogs');
 });
 // Index Route
-app.get("/blogs", (req, res) => {
+app.get('/blogs', (req, res) => {
   // Get all Blog posts from DB
   Blog.find({}, (err, blogs) => {
     if (err) {
-      console.log("err");
+      console.log('err');
     } else {
-      res.render("index", {
+      res.render('index', {
         blogs: blogs,
       });
     }
@@ -62,32 +62,32 @@ app.get("/blogs", (req, res) => {
 });
 
 // New Route
-app.get("/blogs/new", (req, res) => {
-  res.render("new");
+app.get('/blogs/new', (req, res) => {
+  res.render('new');
 });
 
 // Create Route
-app.post("/blogs", (req, res) => {
+app.post('/blogs', (req, res) => {
   // can use middleware
   req.body.blog.body = req.sanitize(req.body.blog.body);
   Blog.create(req.body.blog, (err, blog) => {
     if (err) {
-      res.render("/blogs/new");
+      res.render('/blogs/new');
     } else {
-      res.redirect("/blogs");
-      console.log("new blog added");
+      res.redirect('/blogs');
+      console.log('new blog added');
       console.log(blog);
     }
   });
 });
 
 // Show Route
-app.get("/blogs/:id", (req, res) => {
+app.get('/blogs/:id', (req, res) => {
   Blog.findById(req.params.id, (err, blogPost) => {
     if (err) {
-      res.redirect("/blogs");
+      res.redirect('/blogs');
     } else {
-      res.render("show", {
+      res.render('show', {
         blog: blogPost,
       });
     }
@@ -95,12 +95,12 @@ app.get("/blogs/:id", (req, res) => {
 });
 
 // Edit Route
-app.get("/blogs/:id/edit", (req, res) => {
+app.get('/blogs/:id/edit', (req, res) => {
   Blog.findById(req.params.id, (err, blogPost) => {
     if (err) {
-      res.redirect("/blogs");
+      res.redirect('/blogs');
     } else {
-      res.render("edit", {
+      res.render('edit', {
         blog: blogPost,
       });
     }
@@ -109,12 +109,12 @@ app.get("/blogs/:id/edit", (req, res) => {
 
 // Update Route
 
-app.put("/blogs/:id", (req, res) => {
+app.put('/blogs/:id', (req, res) => {
   // can use middleware
   req.body.blog.body = req.sanitize(req.body.blog.body);
   Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, updatedBlog) => {
     if (err) {
-      res.redirect("/blogs");
+      res.redirect('/blogs');
     } else {
       res.redirect(`/blogs/${req.params.id}`);
     }
@@ -122,16 +122,16 @@ app.put("/blogs/:id", (req, res) => {
 });
 
 // Delete Route
-app.delete("/blogs/:id", (req, res) => {
-  Blog.findByIdAndRemove(req.params.id, (err) => {
+app.delete('/blogs/:id', (req, res) => {
+  Blog.findByIdAndRemove(req.params.id, err => {
     if (err) {
-      res.redirect("/blogs");
+      res.redirect('/blogs');
     } else {
-      res.redirect("/blogs");
+      res.redirect('/blogs');
     }
   });
 });
 
-app.listen(process.env.PORT || 3000, process.env.IP, () => {
-  console.log("Blog away on port 3000");
+app.listen(process.env.PORT || 3050, process.env.IP, () => {
+  console.log('Blog away on port 3000');
 });
